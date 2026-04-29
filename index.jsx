b97786f928fa4fd2d5c291491c8e2c742c9e78cf
Fiqih Badrian <76670473+fiqihbadrian@users.noreply.github.com>
2026-04-30 01:19:44 +0700

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -6,9 +6,9 @@ export function Board() {
 
   const calculateWinner = (squares) => {
     const lines = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8], // Baris
-      [0, 3, 6], [1, 4, 7], [2, 5, 8], // Kolom
-      [0, 4, 8], [2, 4, 6]             // Diagonal
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+      [0, 4, 8], [2, 4, 6]             
     ];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
